178869965581081e08801fe47d34e0d7c9887003
Naor <187169854+NaorVaninu@users.noreply.github.com>
2025-07-30 14:07:39 +0300

    Some extra checks

--- bot.py
+++ bot.py
@@ -42,6 +42,7 @@ def choose_closet_possible(pw):
             if closest_source:
                 return closest_source, planet, net_threat + 1  # +1 for safety
 
+    # offense
     targets = []
     for neut in pw.neutral_planets():
         targets.append((neut, NEUTRAL))
@@ -97,9 +98,10 @@ def do_turn(pw):
     if len(pw.enemy_planets()) == 0:
         return
 
-
     myBest, targetBest, num_ships = choose_closet_possible(pw)
     if myBest is None or num_ships == 0:
+        return
+    if myBest not in pw.my_planets():
         return
 
     #pw.debug('Num Ships: ' + str(num_ships))
