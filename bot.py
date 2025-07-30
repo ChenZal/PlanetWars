0783bd67cbd995c353ef7b2fd6792f900a70fbd4
Naor <187169854+NaorVaninu@users.noreply.github.com>
2025-07-30 12:22:10 +0300

    Fix logic error

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import helper
 
 def do_turn(pw):
+    if len(pw.my_planets()) == 0:
+        return
     (myBest, targetBest, num_ships) = helper.choose_closet_possible(pw)
     pw.debug('Num Ships: ' + str(num_ships))
-    pw.debug(myBest.planet_id() + " " + targetBest.planet_id())
+    pw.debug(str(myBest.planet_id()) + " " + str(targetBest.planet_id()))
     pw.issue_order(myBest, targetBest, num_ships)
--- helper.py
+++ helper.py
@@ -1,27 +1,27 @@
 def choose_closet_possible(pw):
     myPlanets = pw.my_planets()
     isNeut = True
+    targets = []
     if len(pw.neutral_planets()) != 0:
         targets = pw.neutral_planets()
     else:
         targets = pw.enemy_planets()
         isNeut = False
 
-    bestMy = None
-    bestN = None
+    bestMy = myPlanets[0]
+    bestN = targets[0]
     bestDist = 10000000
     num_ships = 0
 
     for myP in myPlanets:
         for n in targets:
-            dist = pw.distance(bestMy, bestN)
+            dist = pw.distance(myP, n)
             needed = n.num_ships()
             if not isNeut:
                 pass
-
             if (dist < bestDist) and needed < myP.num_ships():
                 bestDist = dist
-                bestMy = myPlanets
+                bestMy = myP
                 bestN = n
                 num_ships = needed
 
